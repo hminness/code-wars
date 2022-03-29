fa2298ce76cce84d28970fa1e49a12e8539d5649
hminness <100871706+hminness@users.noreply.github.com>
2022-03-29 17:07:33 -0500

    updates for 3-29

--- code-wars-code/js/main.js
+++ code-wars-code/js/main.js
@@ -1,1 +1,23 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+function reverseWords(str) {
+    let reverseArray = str.replace(/\s/g, '# #').split('#');
+    return reverseArray.map(word => word.split('').reverse().join('')).join('');
+  }
 
+//   In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+const filter_list = l => l.filter(x => typeof x !== 'string');
+
+// Nathan loves cycling.
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+// For example:
+// time = 3 ----> litres = 1
+// time = 6.7---> litres = 3
+const litres = time => Math.floor(time * .5);
