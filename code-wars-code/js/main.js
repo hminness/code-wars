a502ffb8ad5e10815e51664bf00099835d88c7c3
hminness <100871706+hminness@users.noreply.github.com>
2022-04-04 18:38:24 -0500

    new code wars

--- code-wars-code/js/main.js
+++ code-wars-code/js/main.js
@@ -21,3 +21,32 @@ const filter_list = l => l.filter(x => typeof x !== 'string');
 // time = 3 ----> litres = 1
 // time = 6.7---> litres = 3
 const litres = time => Math.floor(time * .5);
+
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples:
+
+// rps('scissors','paper') // Player 1 won!
+// rps('scissors','rock') // Player 2 won!
+// rps('paper','paper') // Draw!
+const rps = (p1, p2) => {
+  if (p1 === p2) {
+    return 'Draw!'
+  } else if (p1 === 'scissors' && p2 === 'paper' ||  
+             p1 === 'paper' && p2 === 'rock' ||
+             p1 === 'rock' && p2 === 'scissors' ) {
+    return 'Player 1 won!'
+  } else {
+    return 'Player 2 won!'
+  }
+};
+
+// Given the a list of numbers, return the list so that the values increment by 1 for each index up to the maximum value.
+function pipeFix(numbers){
+    const newArray = []
+    for (let i = numbers[0]; i <= numbers[numbers.length-1]; i++) {
+      newArray.push(i);
+    }
+    return newArray;
+  }
+
