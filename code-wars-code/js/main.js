2c6bbf1bb229cdb34990db1bfe9f02e4c8142870
hminness <100871706+hminness@users.noreply.github.com>
2022-04-20 22:04:20 -0500

    4-20-22 adds

--- code-wars-code/js/main.js
+++ code-wars-code/js/main.js
@@ -200,3 +200,18 @@ function longest(s1, s2) {
   }
   return s4.split('').sort().join('');
 }
+
+
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+function nbYear(p0, percent, aug, p) {
+  percent = percent/100;
+  let pop = p0;
+  let years = 0;
+  do {
+    pop += (pop * percent) + aug
+    years += 1
+    console.log(years)
+  } while (pop < p)
+  return years;
+}
