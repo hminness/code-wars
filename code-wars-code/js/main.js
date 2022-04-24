47cfdb8dd201993ad774a3c5776b8ef80f06ef36
hminness <100871706+hminness@users.noreply.github.com>
2022-04-23 21:07:52 -0500

    4/23/22 adds

--- code-wars-code/js/main.js
+++ code-wars-code/js/main.js
@@ -218,3 +218,7 @@ function nbYear(p0, percent, aug, p) {
 // Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
 // If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
 const findNextSquare = (sq) => (Math.sqrt(sq) % 1 === 0) ? (Math.sqrt(sq) +1)*(Math.sqrt(sq)+1) : -1; 
+
+// Write a function which calculates the average of the numbers in a given list.
+// Note: Empty arrays should return 0.
+const find_average = array =>  array.length > 0 ? array.reduce((a,b)=>a+b) / array.length : 0;
