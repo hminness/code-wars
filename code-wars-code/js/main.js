fe0a4a12d29ac0b537d1bbaa549973b4c7e1363f
hminness <100871706+hminness@users.noreply.github.com>
2022-04-28 17:46:35 -0500

    4-28-22 adds

--- code-wars-code/js/main.js
+++ code-wars-code/js/main.js
@@ -278,3 +278,23 @@ function openOrSenior(data){
   })
   return status
 }
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+function solution(number){
+  let sum = 0;
+  for (let i = (number-1); i > 2; i--) { 
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i
+    }
+  }
+  return sum
+}
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+function fakeBin(x){
+  let realBinary = '';
+  x.split('').forEach(a=> realBinary +=(a<5 ? 0 : 1 ))
+  return realBinary
+}
