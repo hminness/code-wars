397ff6ad83268a5b30a82a15367792ec315c65cf
hminness <100871706+hminness@users.noreply.github.com>
2022-05-05 17:16:52 -0500

    5/5/22 adds

--- code-wars-code/js/may22.js
+++ code-wars-code/js/may22.js
@@ -59,3 +59,9 @@ function bmi(weight, height) {
 
 // Complete the solution so that the function will break up camel casing, using a space between words.
 const solution = string => string.split(/(?=[A-Z])/).join(' ');
+
+// I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+// P.S. Each array includes only integer numbers. Output is a number too.
+function arrayPlusArray(arr1, arr2) {
+  return arr1.reduce((a,b)=>a+b) + arr2.reduce((a,b)=>a+b); //something went wrong
+}
