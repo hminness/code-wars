f760d0b949c9e1fc9fa9689742fa6617690f8159
hminness <100871706+hminness@users.noreply.github.com>
2022-05-14 19:59:35 -0500

    5/14/22 adds

--- code-wars-code/js/may22.js
+++ code-wars-code/js/may22.js
@@ -190,3 +190,15 @@ function duplicateCount(text){
   }
   return count
 }
+
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+// Mind the input validation.
+function sumArray(array) {
+  if (!array || array.length <= 2) {
+    return 0
+  } else {
+    array = array.sort((a,b)=>a-b)
+    return (array.reduce((c,d)=>c + d)) - (array[0] + array[array.length-1]);
+  }
+}
