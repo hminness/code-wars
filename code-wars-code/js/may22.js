50632cfe2c3463246bc149b76c0a86a434906cc6
hminness <100871706+hminness@users.noreply.github.com>
2022-05-20 19:51:34 -0500

    5/20/22 adds

--- code-wars-code/js/may22.js
+++ code-wars-code/js/may22.js
@@ -263,3 +263,9 @@ function isValidWalk(walk) {
   })
   return count === 0 ? true : false 
 }
+
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+// The binary number returned should be a string.
+function addBinary(a,b) {
+  return (a+b).toString(2)
+}
