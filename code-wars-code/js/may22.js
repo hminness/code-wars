8aaa91f123fcc0ee729c765e17f9745c7225ebf6
hminness <100871706+hminness@users.noreply.github.com>
2022-05-24 17:38:36 -0500

    5/24/22 adds

--- code-wars-code/js/may22.js
+++ code-wars-code/js/may22.js
@@ -296,3 +296,12 @@ decodeMorse = function(morseCode){
     return a.split(' ').map(b=> MORSE_CODE[b]).join('')
   }).join(' ').trim()
 }
+
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+var uniqueInOrder=function(iterable){
+  let array = [];
+  for (const a of iterable) {
+    if (a !== array[array.length-1]) array.push(a)
+  }
+  return array
+}
