b0ab4f3e89fddbba96c3501301a96f8149953406
hminness <100871706+hminness@users.noreply.github.com>
2022-05-29 14:00:00 +0100

    5/29/22 adds

--- code-wars-code/js/may22.js
+++ code-wars-code/js/may22.js
@@ -343,3 +343,10 @@ var number = function(busStops){
   }
   return sum
 }
+
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+// Give your answer as a string matching "odd" or "even".
+// If the input array is empty consider it as: [0] (array with a zero).
+function oddOrEven(array) {
+  return array.length === 0 || array.reduce((a,b)=>a+b) % 2 ===0 ? 'even' : 'odd'
+}
