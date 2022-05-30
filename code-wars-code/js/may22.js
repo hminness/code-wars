14baadee386fe70e486452ec8333f3cde77910c2
hminness <100871706+hminness@users.noreply.github.com>
2022-05-30 14:00:00 +0100

    5/30/22 adds

--- code-wars-code/js/may22.js
+++ code-wars-code/js/may22.js
@@ -350,3 +350,11 @@ var number = function(busStops){
 function oddOrEven(array) {
   return array.length === 0 || array.reduce((a,b)=>a+b) % 2 ===0 ? 'even' : 'odd'
 }
+
+// Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
+var min = function(list){
+  return Math.min(...list)
+}
+var max = function(list){
+  return Math.max(...list)
+}
