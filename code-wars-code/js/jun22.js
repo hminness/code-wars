96c6161c2475cab15679b34a1d108f226de69c8a
hminness <100871706+hminness@users.noreply.github.com>
2022-06-06 14:00:00 +0100

    6/6/22 adds

--- code-wars-code/js/jun22.js
+++ code-wars-code/js/jun22.js
@@ -44,4 +44,20 @@ function smash (words) {
     return string.split(' ');
   }
 
-  
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+function countBy(x, n) {
+    let z = [];
+    for (let i = 1; i <= n; i++) {
+      z.push(i * x)
+    }
+    return z;
+}
+
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Return your answer as a number.
+function sumMix(x){
+    return x.reduce((a,b)=>a + Number(b),0)
+}
+
