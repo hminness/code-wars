375bd58951c0018fe26efa638b72e6a9ffb43d88
hminness <100871706+hminness@users.noreply.github.com>
2022-06-12 14:00:00 +0100

    6/12/22 adds

--- code-wars-code/js/jun22.js
+++ code-wars-code/js/jun22.js
@@ -87,3 +87,9 @@ function removeSmallest(numbers) {
     return [...numbers.slice(0,numIndex), ...numbers.slice(numIndex + 1)]
 }
 
+// Write a function that returns both the minimum and maximum number of the given list/array.
+// All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.  
+function minMax(arr){
+  return [Math.min(...arr),Math.max(...arr)];
+}
+
