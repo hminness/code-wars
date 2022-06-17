ea55c0d6ccf7d8bacac0265e0a6e1418715c7052
hminness <100871706+hminness@users.noreply.github.com>
2022-06-17 14:00:00 +0100

    6/17/22 adds

--- code-wars-code/js/jun22.js
+++ code-wars-code/js/jun22.js
@@ -128,3 +128,12 @@ function getDivisorsCnt(n){
   }
   return count
 }
+
+// Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' 
+function divisors(integer) {
+  let array = [];
+  for (let i = 2; i < integer; i++) {
+    if (integer % i === 0) array.push(i)
+  }
+  return array.length > 0 ? array : `${integer} is prime`
+};
